093e6bbbed650944043f44a62827c419ba80f87f
최석균 <syaku@naver.com>
2018-04-26 01:27:01 +0900

    webpack basic

---
+++ README.md
@@ -0,0 +1,5 @@
+# Webpack Example
+
+### 웹팩 설치와 설정 : Webpack install & setting #1
+
+http://syaku.tistory.com/362  
---
+++ basic/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js?hash=[hash]',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new ExtractTextPlugin('[name].css?hash=[hash]'),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+        }),
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
